abf23d9dc3cca208fc549946a9e9edd3688d1a18
Gaurav Shukla <gauravshukla127001@gmail.com>
2026-07-04 13:28:42 +0000

    Correct project name

--- build.zig
+++ build.zig
@@ -44,7 +44,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "tt",
+        .name = "text_renderer_zig",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
